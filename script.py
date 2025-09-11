af99d528448175257f546131954d68e0f3643e8c
Aidsan58 <aidanr1@comcast.net>
2025-09-11 14:47:28 -0700

    added i3 subcommand

--- script.py
+++ script.py
@@ -1,7 +1,14 @@
 import os
 
-directory_path = "~/Pictures/Wallpapers"
+def run_i3_command(wallpaper: str):
+    import subprocess
+    command = f"feh --bg-fill {directory_path}{wallpaper}"
+    subprocess.run(["i3-msg", command])
+
+directory_path = "~/Pictures/Wallpapers/"
 
 def wallpaper_swap():
     for item_name in os.listdir(directory_path):
         print(item_name)
+
+
